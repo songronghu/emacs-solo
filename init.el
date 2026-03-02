d4e85de28987718489624071d3ab05b178497859
Rahul Martim Juliato <rahul.juliato@gmail.com>
2026-03-02 15:27:36 -0300

    feat: duck.ai to webjump config

--- init.el
+++ init.el
@@ -3140,6 +3140,7 @@ As seen on: https://emacs.dyerdwelling.family/emacs/20250604085817-emacs--buildi
   :custom
   (webjump-sites
    '(("DuckDuckGo" . [simple-query "www.duckduckgo.com" "www.duckduckgo.com/?q=" ""])
+     ("DuckDuckAI" . [simple-query "duck.ai" "duck.ai/?q=" ""])
      ("Google" . [simple-query "www.google.com" "www.google.com/search?q=" ""])
      ("YouTube" . [simple-query "www.youtube.com/feed/subscriptions" "www.youtube.com/results?search_query=" ""])
      ("ChatGPT" . [simple-query "https://chatgpt.com" "https://chatgpt.com/?q=" ""]))))
